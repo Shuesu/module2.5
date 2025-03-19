042a2ea7dbf29f3c255c02b063f02934e0f0ab64
Atabayev Ali <sptmbrw@gmail.com>
2025-03-19 18:46:18 +0500

    task2

--- src/api/api.js
+++ src/api/api.js
@@ -1,31 +1,24 @@
-import { HTTP_METHOD } from '../constants'
+import { HTTP_METHOD } from '../constants';
 
-const fetchServer = (method, { id, ...payload } = {}, params = '') => {
-
+const fetchServer = (method, { id, ...payload } = {}) => {
    let url = 'http://localhost:3006/todos';
    let options = {
       method,
       headers: { 'Content-Type': 'application/json' },
    };
 
-   if (method !== HTTP_METHOD.GET) {
-      url += '';
-   } else {
-      if (method !== HTTP_METHOD.POST) {
-         url += `/${id}`;
-      }
-
-      if (method !== HTTP_METHOD.DELETE) {
-         options.body = JSON.stringify(payload);
-      }
+   if (id && method !== HTTP_METHOD.POST) {
+      url += `/${id}`;
    }
 
+   if (method !== HTTP_METHOD.GET && method !== HTTP_METHOD.DELETE) {
+      options.body = JSON.stringify(payload);
+   }
 
-   return fetch(url, options)
-      .then((jsonData) => jsonData.json());
-}
+   return fetch(url, options).then((response) => response.json());
+};
 
-export const createTodo = (newTodo) => fetchServer('POST', newTodo);
-export const readTodos = () => fetchServer('GET');
-export const updateTodo = (todoData) => fetchServer('PATCH', todoData);
-export const deleteTodo = (todoId) => fetchServer('DELETE', { id: todoId });
+export const createTodo = (newTodo) => fetchServer(HTTP_METHOD.POST, newTodo);
+export const readTodos = () => fetchServer(HTTP_METHOD.GET);
+export const updateTodo = (todoData) => fetchServer(HTTP_METHOD.PATCH, todoData);
+export const deleteTodo = (todoId) => fetchServer(HTTP_METHOD.DELETE, { id: todoId });
